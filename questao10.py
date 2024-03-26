f84d0aaf0dde56af2a847244283d776d8ee02d1d
JeffSSampaio <165072019+JeffSSampaio@users.noreply.github.com>
2024-03-26 19:25:57 -0300

    Update questao10.py
    alterado 
    verificar ganhador()
    indicarposicao()
    jogar()

--- questao10.py
+++ questao10.py
@@ -2,10 +2,6 @@ from random import randint
 linha = 3
 coluna = 3
 jogoDaVelha = [0] * linha
-jogadorX = 1 
-coordenadas = []
-jogadorY = -1
-pontuacao = []
 for l in range(len(jogoDaVelha)):
     jogoDaVelha[l] = [0] * coluna
   
@@ -14,115 +10,42 @@ def imprimirTabuleiro():
         print(jogoDaVelha[l]) 
         
 def verificarGanhador():
-     n = len(jogoDaVelha)
-     verificado = False 
-     
-     for l in range(n): 
-        for c in range(n):   
-         if jogoDaVelha[l] == 1:
-            print(f"\nJogador Y ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break
-         elif jogoDaVelha[l] == -1:
-            print(f"\nJogador X ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break    
-         else:
-            verificado = False
-            break
-            
-     if verificado:
-        for c in range(n):
-            soma = 0
-        for l in range(n):
-         if jogoDaVelha[l] == 1:
-            print(f"\nJogador Y ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break
-         elif jogoDaVelha[l] == -1:
-            print(f"\nJogador X ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break    
-         else:
-            verificado = False
-            break
-                         
-     if verificado: 
-        for d in range(n):
-            if jogoDaVelha[l][l] == 1:
-               print(f"\nJogador Y ganhou!!!! ")
-               imprimirTabuleiro()
-               verificado = True
-               break
-            elif jogoDaVelha[l][l] == -1:
-               print(f"\nJogador X ganhou!!!! ")
-               imprimirTabuleiro()
-               verificado = True
-               break    
-            else:
-               verificado = False
-               break
-                
-     if verificado:
-        for d in range(n):     
-          if jogoDaVelha[l][n-1-l] == 1:
-            print(f"\nJogador Y ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break
-          elif jogoDaVelha[l][n-1-l] == -1:
-            print(f"\nJogador X ganhou!!!! ")
-            imprimirTabuleiro()
-            verificado = True
-            break    
-          else:
-            verificado = False
-            break  
+    for i in range(3):
+        if jogoDaVelha[i][0] == jogoDaVelha[i][1] == jogoDaVelha[i][2] != 0 or \
+           jogoDaVelha[0][i] == jogoDaVelha[1][i] == jogoDaVelha[2][i] != 0:
+            return True
 
-def inserirPosicao(vez): 
-    for i in range(1):
+
+    if jogoDaVelha[0][0] == jogoDaVelha[1][1] == jogoDaVelha[2][2] != 0 or \
+       jogoDaVelha[0][2] == jogoDaVelha[1][1] == jogoDaVelha[2][0] != 0:
+        return True
+
+    return False
+
+def inserirPosicao(linha,coluna,valor): 
+       if 0 <= linha < len(jogoDaVelha) and 0 <= coluna < len(jogoDaVelha[0]):
+          jogoDaVelha[linha][coluna] = valor
+          return True
+       return False    
+       
+def jogar():
+   vez = randint(0,1)
+   jogador_atual = 1 if vez == 0 else 1
+   while True:
+     imprimirTabuleiro()
+     print(f"Jogador {jogador_atual} sua vez:")
      linha = int(input("Digite a linha da coordenada que deseje inserir : "))
      coluna = int(input("Digite a coluna da coordenada que deseje inserir: "))
-     coordenadas.append((linha, coluna)) 
-    for coord in coordenadas:
-     linha, coluna = coord 
-    
-     if 0 <= linha < len(jogoDaVelha) and 0 <= coluna < len(jogoDaVelha[0]):
-        if vez == 1:
-         jogoDaVelha[linha][coluna] = 1
-         valor = jogoDaVelha[linha][coluna]
-         print(f"Coordenada ({linha}, {coluna}): {valor}")
-         imprimirTabuleiro()
-         vez = randint(-1,1)
-         return vez
-        elif vez == -1:
-         jogoDaVelha[linha][coluna] = -1
-         valor = jogoDaVelha[linha][coluna]
-         print(f"Coordenada ({linha}, {coluna}): {valor}")
-         imprimirTabuleiro()
-         vez = randint(-1,1)
-         return vez
-         
+     if inserirPosicao(linha,coluna,jogador_atual):
+        ganhador = verificarGanhador()
+        if ganhador != False:
+          imprimirTabuleiro()
+          print(f"Jogador {jogador_atual} ganhou!")
+          break
+        jogador_atual = -jogador_atual  
      else:
-       print(f"Não é possivel encontrar coordenadas ({linha,coluna}) no Tabuleiro") 
-def jogar():
-   imprimirTabuleiro()
-   while True:
-    vez = randint(-1,1)
-    if vez == 1:
-        print("Jogador X, sua vez")
-        inserirPosicao(1)
-        verificarGanhador()
-    elif vez == -1:
-            print("Jogador Y, sua vez:\n")
-            inserirPosicao(-1)
-            verificarGanhador()
-    else:
-         break
+          print("Posição invalida")
+
                      
    
        
